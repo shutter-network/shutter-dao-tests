68ca0ca7c766120784e80c23417b4adb5a78484a
Jannik Luhn <jannik.luhn@posteo.de>
2024-08-20 17:31:36 +0200

    Update solidity version

--- test/TreasuryManagementTemp.sol
+++ test/TreasuryManagementTemp.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.19;
+pragma solidity >=0.8.19;
 
 import { ProposalTest } from "../utils/ProposalTest.sol";
 import { Vm } from "forge-std/Vm.sol";
--- utils/ProposalTest.sol
+++ utils/ProposalTest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.19;
+pragma solidity >=0.8.19;
 
 import {Test, console2, stdStorage, StdStorage} from "forge-std/Test.sol";
 import "forge-std/Vm.sol";
